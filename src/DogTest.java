d82c97ef068c0ade55b7a049a64472dc9f9c6fd2
SolomonVP <Lum7023@yandex.ru>
2022-11-03 04:46:23 +0300

    master

---
+++ src/DogTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+        Dog dog = new Dog("Albert", 2);
+        assertEquals("Albert", dog.getName());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+}
